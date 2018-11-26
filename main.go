b96246878871b264c705573dfbc8c5d762ad0018
J Forster <johannforster15@gmail.com>
2018-11-26 15:13:51 +0100

    added periph

--- main.go
+++ main.go
@@ -5,27 +5,41 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"time"
+
+	"periph.io/x/periph/conn/gpio"
+	"periph.io/x/periph/conn/gpio/gpioreg"
+	"periph.io/x/periph/host"
+	_ "periph.io/x/periph/host/rpi"
 
 	"github.com/j-forster/Wazihub-API"
 )
 
+var deviceId = wazihub.CurrentDeviceId()
+
 func main() {
+	host.Init()
 
 	log.Println("Wazihub Raspberri Pi Demo")
 
 	//////////
 
 	// See 'device.json'.
-	file, _ := ioutil.ReadFile("device.json")
+	file, err := ioutil.ReadFile("device.json")
+	if err != nil {
+		log.Fatal(err)
+	}
 	// The ID of the device this program is running on.
 	// We use 'CurrentDeviceId' to create a unique Id for this device.
-	deviceId := wazihub.CurrentDeviceId()
 	log.Println("This device id is:", deviceId)
 
 	// Create a new Device ...
 	device := &wazihub.Device{Id: deviceId}
 	// ... with the data from 'device.json'
-	json.Unmarshal(file, device)
+	err = json.Unmarshal(file, device)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	//////////
 
@@ -37,7 +51,7 @@ func main() {
 	//////////
 
 	// We register the device, even though it might already be registered ...
-	err := wazihub.CreateDevice(device)
+	err = wazihub.CreateDevice(device)
 	if err != nil {
 		log.Fatalln("Failed to register!", err)
 	}
@@ -45,13 +59,12 @@ func main() {
 
 	//////////
 
-	ioutil.WriteFile("/sys/class/gpio/export", []byte("18"), 0666)
-	ioutil.WriteFile("/sys/class/gpio/gpio18/direction", []byte("out"), 0666)
-
-	ioutil.WriteFile("/sys/class/gpio/export", []byte("23"), 0666)
-	ioutil.WriteFile("/sys/class/gpio/gpio23/direction", []byte("out"), 0666)
+	go ButtonListener()
 
 	//////////
+
+	gpio18 := gpioreg.ByName("18")
+	gpio23 := gpioreg.ByName("23")
 
 	// Let's hook into the actuation.
 	led1, _ := wazihub.Actuation(deviceId, "led1")
@@ -63,11 +76,26 @@ func main() {
 		select {
 		case action := <-led1:
 			log.Println("LED 1:", action)
-			ioutil.WriteFile("/sys/class/gpio/gpio18/value", []byte(action), 0666)
+			gpio18.Out(gpio.Level(action == "1"))
 
 		case action := <-led2:
 			log.Println("LED 2:", action)
-			ioutil.WriteFile("/sys/class/gpio/gpio23/value", []byte(action), 0666)
+			gpio23.Out(gpio.Level(action == "1"))
+		}
+	}
+}
+
+//////////
+
+func ButtonListener() {
+
+	gpio24 := gpioreg.ByName("24")
+	// Connect at push-button to GPIO 24
+	gpio24.In(gpio.PullUp, gpio.BothEdges)
+
+	for {
+		if gpio24.WaitForEdge(time.Second * 30) {
+			wazihub.PostValue(deviceId, "push-button", gpio24.Read())
 		}
 	}
 }
